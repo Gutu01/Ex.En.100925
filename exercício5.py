d090d223c0ac9f68a786fe63cc881e2779345c94
Gutu01 <gustavomt058@gmail.com>
2025-09-03 10:44:19 -0400

    Terminei

---
+++ exercício5.py
@@ -0,0 +1,14 @@
+nome = input("Digite o nome:")
+disciplina = input("Nome da disciplina:")
+nota1 =  float(input("Nota da 1º prova:"))
+nota2 =  float(input("Nota da 2º prova:"))
+nota3 =  float(input("Nota da 3º prova:"))
+
+media = (nota1+nota2+nota3)/3
+
+print("Nome: ", nome)
+print("Disciplina: ", disciplina)
+print("1º prota: ", nota1)
+print("2º prota: ", nota2)
+print("3º prota: ", nota3)
+print("Média do aluno: ", media)
