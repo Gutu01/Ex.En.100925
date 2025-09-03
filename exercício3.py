3c029ca2e354c6ab11faa4fae2b7aa10c8468f3c
Gutu01 <gustavomt058@gmail.com>
2025-09-03 10:35:15 -0400

    Terminei

---
+++ exercício3.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome:")
+titulo = input("Digite o título eleitoral:")
+num_candidato = int(input("Digite o número do candidato:"))
+
+print("\nNome: ", nome)
+print("Título eleitoral: ", titulo)
+print("Número do candidato:", num_candidato)
