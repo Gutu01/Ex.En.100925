8234f980064497368e8218d2d7f5bc64e1647829
Gutu01 <gustavomt058@gmail.com>
2025-09-03 10:39:11 -0400

    Terminei

---
+++ exercício4.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome:")
+placa = input("Digite a placa do carro:")
+modelo = input("Digite o modelo:")
+cor = input("Digite a cor do carro:")
+
+print("\nNome: ", nome)
+print("Placa do carro: ", placa)
+print("Modelo do carro: ", modelo)
+print("Cor do carro: ", cor)
